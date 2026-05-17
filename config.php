1eb99fc88cc0dcb5388bee8a20fdc83d7ebe65d6
dingkanglee <dingkang.lee@qiu.edu.my>
2026-05-17 20:56:33 +0800

    Update homepage and booking page

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
-$conn = mysqli_connect(
-    "localhost",
-    "root",
-    "",
-    "hotel_reservation"
-);
+$host = "sql102.infinityfree.com";
+$username = "if0_41943480";
+$password = "4PzhLJvRew";
+$database = "if0_41943480_dbkang";
+
+$conn = mysqli_connect($host, $username, $password, $database);
 
 if(!$conn){
-    die("Connection Failed");
+    die("Database connection failed: " . mysqli_connect_error());
 }
 
 ?>
